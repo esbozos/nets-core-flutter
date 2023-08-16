997358ec364dd0201bc9b8b33bc1a124762f995f
Norman Torres <norman.nets@gmail.com>
2023-08-16 10:46:31 -0500

    device identifier, v0.15

--- lib/nets_core.dart
+++ lib/nets_core.dart
@@ -1,16 +1,16 @@
+// ignore_for_file: unused_import
+
 library nets_core;
+
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nets_core/services/api_urls.dart';
+import 'package:nets_core/services/network_service.dart';
+import 'package:nets_core/services/storage_service.dart';
+import 'package:nets_core/utils/device_utils.dart';
 
 export 'services/storage_service.dart';
 
-class NetsCore {
-  static const MethodChannel _channel = const MethodChannel('store_checker');
-
-  static Future<bool> get isTrusted async {
-    final String? sourceName = await _channel.invokeMethod('getSource');
-    debugPrint('isTrusted sourceName: $sourceName');
-    return true;
-  }
-}
+class NetsCore {}
