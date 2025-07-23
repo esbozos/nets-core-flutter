935e5716b7da1885163e0bed830125df362e84ca
NORMAN TORRES <norman.nets@gmail.com>
2025-07-23 10:24:25 +0200

    localizations

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,46 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get requiredField => 'This field is required';
+
+  @override
+  String get country => 'Country';
+
+  @override
+  String get submit => 'Submit';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get picturePreview => 'Picture Preview';
+
+  @override
+  String get picture => 'Picture';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get description => 'Description';
+
+  @override
+  String get yes => 'Yes';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get retakePicture => 'Retake Picture';
+
+  @override
+  String get continueNext => 'Continue';
+}
