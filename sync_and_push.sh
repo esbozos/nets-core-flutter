0c271b5d6b63e702ce609d91d415aeb5b7d5b9df
NORMAN TORRES <norman.nets@gmail.com>
2025-08-27 14:46:50 +0200

    version: 0.1.3

--- sync_and_push.sh
+++ sync_and_push.sh
@@ -11,22 +11,17 @@
 version=$(grep "version:" pubspec.yaml | cut -d ' ' -f 2)
 # log to the console
 echo "current version is $version"
-
 # get the patch number from the version number and increment it
 patch=$(echo $version | cut -d '.' -f 3 | awk '{print $1+1}')
-
-
 # change version patch number
 # keep major and minor version number
 # example: 0.1.0 -> 0.1.1
 new_version=$(echo $version | cut -d '.' -f 1,2).$patch
 echo "new version is $new_version"
 echo "old $version"
-
 # replace the old version number with the new version number
 sed -i '' "s/version: 0.1.1/version: 0.1.2/g" pubspec.yaml &> /dev/null
 # the previous command is not working on mac, not change the version number
-
 # log to the console
 echo "pubspec.yaml file updated"
 # commit the changes
